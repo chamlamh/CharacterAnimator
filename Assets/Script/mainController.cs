bc147fc7620b1f682b93fc5b59f50bfb0dbff70e
chamlamh <70345325+chamlamh@users.noreply.github.com>
2021-02-28 00:53:43 -0500

    Initial commit

---
+++ Assets/Script/mainController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mainController : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal");
+        anim.SetFloat("speed", move);
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * move * Time.deltaTime;
+        transform.position = position;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("jump");
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, 15f), ForceMode2D.Impulse);
+        }
+    }
+}
